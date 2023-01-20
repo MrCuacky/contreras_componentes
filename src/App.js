717955dba119de886c7115b5a8e02e84add8634b
MrCuacky <100604780+MrCuacky@users.noreply.github.com>
2023-01-19 23:52:30 -0600

    v1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+const Button = styled.button`
+  /* Adapt the colors based on primary prop */
+  background: ${props => props.primary ? "palevioletred" : "white"};
+  color: ${props => props.primary ? "white" : "palevioletred"};
+
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+  height:100px;
+  width:200px;
+
+`;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <div className='links'>
+      <a href='https://utd.edu.mx'>
+        <Button name='utd'> UTD</Button>
+      </a>  
+     
+        <a href='https://github.com/MrCuacky'>
+        <Button name='git'>GitHub</Button>
+        </a>  
+        
+        <a href='https://twitter.com/MrCuacky12'>
+        <Button name='twitter'>Twitter</Button >
         </a>
-      </header>
-    </div>
+       
+        </div>
+      </div>
   );
 }
 
