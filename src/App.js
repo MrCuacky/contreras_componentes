13e27422dc4661724d06aabdd9795898b3b39285
MrCuacky <100604780+MrCuacky@users.noreply.github.com>
2023-02-02 10:47:53 -0600

    02/02

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
 import styled from 'styled-components'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import GoogleMap from './GoogleMap';
+import Carrusel from './Carrusel'
 
 const Button = styled.button`
   /* Adapt the colors based on primary prop */
-  background: ${props => props.primary ? "palevioletred" : "white"};
-  color: ${props => props.primary ? "white" : "palevioletred"};
+  background: ${props => props.primary ? "purple" : "#e6e6e6"};
+  color: ${props => props.primary ? "#555555 " : "purple"};
 
   font-size: 1em;
   margin: 1em;
   padding: 0.25em 1em;
   border: 2px solid palevioletred;
   border-radius: 3px;
-  height:100px;
-  width:200px;
+  height:50px;
+  width:100px;
+  background-color:#de8ede;
 
 `;
 
 function App() {
   return (
     <div className="App">
+
+    <div className='nomaps'>
+
+    <div className='text'>
+    <h1>Juan Pablo Contreras Vásquez</h1>
+    <h2>Bienvenid@ a mi <span>LINKTREE</span> </h2>
+    </div>
+
       <div className='links'>
       <a href='https://utd.edu.mx'>
         <Button name='utd'> UTD</Button>
@@ -35,6 +44,15 @@ function App() {
         </a>
        
         </div>
+        </div>
+
+      <div className='carrusel'>
+        <Carrusel></Carrusel>
+      </div>
+
+      <>
+        <GoogleMap></GoogleMap>
+      </>
       </div>
   );
 }
